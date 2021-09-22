854c6cc64c6e642a7d2f85bed2f1189e68e7f116
Ashutosh2504 <ashup953@gmail.com>
2021-09-22 15:00:29 +0530

    initial commit

---
+++ src/main/java/com/demo/maven/App.java
@@ -0,0 +1,18 @@
+package com.demo.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        showMessage(args[0]);
+    }
+    public static void showMessage(String msg)
+    {
+    	System.out.println(msg);
+    }
+}
